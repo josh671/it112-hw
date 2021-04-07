1034db3d71db05157f7e6276e1bc72059ed26399
josh671 <jmb671@hotmail.com>
2021-04-07 15:09:30 -0700

    adding assignment 3

--- PythonClub1/Club/urls.py
+++ PythonClub1/Club/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path 
+#2. adding import for views 4/7/2021 
+from . import views 
 
 urlpatterns = [
-    
+    #3. adding path to get index.html 4-7-2021 
+    path('', views.index, name='index'), 
+
 ]
