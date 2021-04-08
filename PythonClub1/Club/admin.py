dbc7d4c12fa2043ed26f0b80a00619002a6c00d7
josh671 <jmb671@hotmail.com>
2021-04-08 14:25:47 -0700

    adding assignment4

--- PythonClub1/Club/admin.py
+++ PythonClub1/Club/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+#7 importing models 
+from .models import Meeting, MeetingMinutes, Resources, Event 
+
 
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(MeetingMinutes)
+admin.site.register(Resources)
+admin.site.register(Event)
